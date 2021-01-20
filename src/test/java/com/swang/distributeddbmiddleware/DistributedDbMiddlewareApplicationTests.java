9e22abfebbefbe6a742fd15ce95d35d38e0fe010
ming.wang4 <ming.wang4@envision-digital.com>
2021-01-21 01:31:11 +0800

    init sharding jdbc

--- src/test/java/com/swang/distributeddbmiddleware/DistributedDbMiddlewareApplicationTests.java
+++ src/test/java/com/swang/distributeddbmiddleware/DistributedDbMiddlewareApplicationTests.java
@@ -1,7 +1,6 @@
 package com.swang.distributeddbmiddleware;
 
 
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.swang.ddbm.helo.entity.User;
 import com.swang.ddbm.helo.mapper.UserMapper;
@@ -10,23 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DistributedDbMiddlewareApplicationTests {
 
-	@Autowired
-	private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
-	@Test
-	public void testSelect() {
-		System.out.println(("----- selectAll method test ------"));
-		QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-		queryWrapper.lambda().gt(User::getAge,21);
-		List<User> userList = userMapper.selectList(queryWrapper);
-		assertEquals(2, userList.size());
-		userList.forEach(System.out::println);
-	}
+    @Test
+    public void testAddUser() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(("----- Add user ------"));
+            User user = new User();
+            user.setAge(i+ new Random(17).nextInt(90));
+            user.setEmail("a"+i+"@email.com");
+            user.setName("a"+i);
+            System.out.println(user);
+            userMapper.insert(user);
+        }
+    }
+
+    @Test
+    public void testSelect() {
+        System.out.println(("----- selectAll method test ------"));
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+		queryWrapper.lambda().eq(User::getAge,32);
+        List<User> userList = userMapper.selectList(queryWrapper);
+       // assertEquals(0, userList.size());
+        userList.forEach(System.out::println);
+    }
 
 }
